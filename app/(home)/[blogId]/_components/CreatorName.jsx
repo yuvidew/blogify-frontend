3786c11e83927b83d3cea42a05fa2ef8766c84ee
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-25 16:12:52 +0530

    update file

--- app/(home)/[blogId]/_components/CreatorName.jsx
+++ app/(home)/[blogId]/_components/CreatorName.jsx
@@ -5,7 +5,7 @@ import { CircleUserRound } from 'lucide-react'
 import React, { useEffect } from 'react'
 
 export const CreatorName = ({userId}) => {
-    const [data , fetchData] = useFetch(null)
+    const [data , fetchData] = useFetch()
 
     useEffect(() => {
         fetchData(`https://blogify-server-j4lx.onrender.com/api/get/user/${userId}`)
@@ -15,7 +15,7 @@ export const CreatorName = ({userId}) => {
         <div>
             <div className=' flex items-center gap-2 text-stone-400'>
                 <CircleUserRound className='p-0.5 w-[1.5rem] h-[1.5rem] rounded-full bg-stone-200 text-black' />
-            {!data ? (
+            {data ? (
                 <h5>{data.name}</h5>
             ) : (
                 <Skeleton className={"h-[2rem] w-32"}></Skeleton>
