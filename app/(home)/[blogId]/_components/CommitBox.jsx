607ab5306e771438a85a0737d4e6e38710875473
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-30 01:28:27 +0530

    update commitBox comp

--- app/(home)/[blogId]/_components/CommitBox.jsx
+++ app/(home)/[blogId]/_components/CommitBox.jsx
@@ -41,36 +41,44 @@ export const CommitBox = ({
 
 
     return (
-        <section className=' pb-10'>
-        <h1 className=' mt-3 text-[1.2rem]'>Enter your Commit</h1>
-            <div className=' flex items-center gap-4 mt-2 lg:w-[50%]'>
-                <Input 
-                    placeholder = "Add a commit.." 
-                    className = "bg-slate-50 rounded-full placeholder:text-stone-700 text-stone-900"
-                    value = {commit}
-                    onChange = {(e) => setCommit(e.target.value)} 
-                />
-                <SendHorizontal onClick={onSubmit} className=' cursor-pointer text-stone-400'  />
+        <section className=' pb-10  mt-4 lg:flex items-start gap-3'>
+            <div className='lg:w-[50%]'>
+                <h1 className=' mt-3 text-[1.2rem] '>Enter your Commit</h1>
+                <div className=' flex items-center gap-4 mt-2 w-[90%]'>
+                    <Input 
+                        placeholder = "Add a commit.." 
+                        className = "bg-slate-50 rounded-full placeholder:text-stone-700 text-stone-900"
+                        value = {commit}
+                        onChange = {(e) => setCommit(e.target.value)} 
+                    />
+                    <SendHorizontal onClick={onSubmit} className=' cursor-pointer text-stone-400'  />
+                </div>
             </div>
-            {!isLoading  ? (
-                <ul className=' bg-stone-800 px-4 py-3 rounded-md mt-5'>
-                    {data.map((ele  , index) => (
-                        <li key={index} className=' mb-3 border-b border-b-stone-700 py-8'>
-                            <div className=' flex items-start gap-4'>
-                                <div>
-                                    <User className='p-2 rounded-full bg-stone-400 text-stone-900 w-[3rem] h-[3rem]' />
+            <div className='lg:w-[50%]'>
+                {!isLoading  ? (
+                    <ul className='  mt-5'>
+                        {data.map((ele  , index) => (
+                            <li key={index} className=' bg-stone-100 dark:bg-stone-900 mb-3 px-4 rounded-md  border-b-stone-700 py-8'>
+                                <div className=' flex items-start gap-4'>
+                                    <div>
+                                        <User className='p-2 rounded-full bg-stone-400 text-stone-900 w-[2.5rem] h-[2.5rem]' />
+                                    </div>
+                                    <div>
+                                        <p className=' text-stone-500'>{ele.name}</p>
+                                        <p className=' text-[1.1rem]'>{ele.text}</p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <p className=' text-stone-500'>{ele.name}</p>
-                                    <p className=' text-[1.1rem]'>{ele.text}</p>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <Skeleton className={'h-[4rem] w-full'} />
-            )}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <div className='  mt-5'>
+                        {[1, 2 ,3 , 4,5].map(ele => (
+                            <Skeleton key={ele} className={'h-[3rem] w-full mb-3 px-4 rounded-md  border-b-stone-700 py-8'} />
+                        ) )}
+                    </div>
+                )}
+            </div>
         </section>
     )
 }
